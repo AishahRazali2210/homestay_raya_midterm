506241e2cdb06792cd5aa65576020c382d8e6c45
AishahRazali2210 <aishahaesherrazali@gmail.com>
2023-01-31 10:59:46 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/homestay_raya_midterm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.homestay_raya_midterm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/config.dart
@@ -0,0 +1,3 @@
+class CONFIG {
+  static const String server = "http://10.19.99.125/homestay_raya_midterm";
+}
